ddb7fdee4f3a27b38ec193d6494ad2e066e92107
Mike Little <mike@zed1.com>
2017-02-27 18:16:46 +0000

    New functionality. Requires Advanced Custom Fields plugin.

--- functions.php
+++ functions.php
@@ -15,4 +15,19 @@ function my_title_filter( $title ) {
 add_action( 'wp_head', 'my_head_action', 99 );
 function my_head_action() {
     echo '<!-- Mike Woz ere -->';
+    echo '<style>.post-subtitle { font-size: 150%; font-weight: bold; text-align: center; margin-bottom: 1em;} </style>';
 }
+
+
+/* Add a filter to retrieve and display the Post subtitle */
+add_filter( 'the_content', 'add_post_subtitle' );
+function add_post_subtitle( $content ) {
+    error_log( "sub_title=". $sub_title );
+    if ( function_exists( 'get_field' ) ) {
+        $sub_title = get_field( 'sub_title' );
+        error_log( "sub_title=". $sub_title );
+        return '<div class="post-subtitle">' . esc_html( $sub_title ) . '</div>' . $content;
+    }
+    return $content;
+}
+
